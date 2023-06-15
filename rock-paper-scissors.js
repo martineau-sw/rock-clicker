6e05eeb8ea3d5de3a1ca1d2a2d1cde790d51b8ff
Seth Martineau <martineausw@gmail.com>
2023-06-14 20:42:01 -0400

    Implement basic score tracking best of 5

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,49 +34,75 @@ function getPlayerChoice() {
     }
 }
 
-function playRound()
-{
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-    if(playerSelection == computerSelection)
+function game() {
+    playRound(getPlayerChoice(), getComputerChoice());
+
+    if(playerScore == 3)
+    {
+        console.log("PlAYER WINS");
+    }
+    else if(computerScore == 3)
+    {
+        console.log("COMPUTER WINS");
+    } 
+    else {
+        console.log(`Player: ${playerScore}
+        Computer: ${computerScore}`)
+        game();   
+    }
+}
+
+function playRound(playerChoice, computerChoice)
+{
+    if(playerChoice == computerChoice)
     {
         console.log("IT'S A TIE");
     }
 
-    else if(playerSelection == "scissors") {
-        switch(computerSelection)
+    else if(playerChoice == "scissors") {
+        switch(computerChoice)
         {
             case "rock":
                 console.log("COMPUTER WINS");
+                computerScore++;
                 break;
             case "paper":
                 console.log("PLAYER WINS");
+                playerScore++;
                 break;
         }
     }
 
-    else if(playerSelection == "paper") {
-        switch(computerSelection)
+    else if(playerChoice == "paper") {
+        switch(computerChoice)
         {
             case "scissors":
                 console.log("COMPUTER WINS");
+                computerScore++;
                 break;
             case "rock":
                 console.log("PLAYER WINS");
+                playerScore++;
                 break;
         }
     }
 
-    else if(playerSelection == "rock") {
-        switch(computerSelection)
+    else if(playerChoice == "rock") {
+        switch(computerChoice)
         {
             case "paper":
                 console.log("COMPUTER WINS");
+                computerScore++;
                 break;
             case "scissors":
                 console.log("PLAYER WINS");
+                playerScore++;
                 break;
         }
     }
 }
+
+game();
